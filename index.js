1af545c914107ae4d208a48e8128216ec2cdf60b
WasefUllah <wasefudvash@gmail.com>
2025-05-25 21:56:13 +0600

    added dynamic get

--- index.js
+++ index.js
@@ -30,6 +30,18 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      
+      
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await taskCollection.findOne(query);
+      console.log(result);
+      
+    });
+
     app.post("/tasks", async (req, res) => {
       const newTask = req.body;
       const result = await taskCollection.insertOne(newTask);
@@ -37,12 +49,10 @@ async function run() {
     });
 
     app.delete("/tasks/:id", async (req, res) => {
- 
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await taskCollection.deleteOne(query);
       res.send(result);
-      
     });
 
     // Send a ping to confirm a successful connection
